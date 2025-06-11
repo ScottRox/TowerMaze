bc6ea1002ae4bdf21c89721713dd7d4aee75346d
scott <scott_roxburgh@hotmail.com>
2025-06-11 12:50:22 +1200

    Initial commit

---
+++ src/main/java/uglyrunt/towermaze/TowerMaze.java
@@ -0,0 +1,14 @@
+package uglyrunt.towermaze;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TowerMaze implements ModInitializer {
+	public static final String MOD_ID = "towermaze";
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/uglyrunt/towermaze/TowerMazeClient.java
@@ -0,0 +1,10 @@
+package uglyrunt.towermaze;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TowerMazeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
